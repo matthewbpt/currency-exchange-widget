3fad9a437507a8205df74a6d503d4a5246de2e6b
Matthew Tompsett <mt@cargonexx.de>
2019-06-21 12:13:52 +0200

    latest

---
+++ precache-manifest.53d1550fe5c57f69bf7cdcd540e6bb65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e937c83895b50ae9ea338f3861f2f5cc",
+    "url": "/currency-exchange-widget/index.html"
+  },
+  {
+    "revision": "6a7e6be6af1045fbb135",
+    "url": "/currency-exchange-widget/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "fbbf3fe2d67d775d2daa",
+    "url": "/currency-exchange-widget/static/css/main.862b9d58.chunk.css"
+  },
+  {
+    "revision": "6a7e6be6af1045fbb135",
+    "url": "/currency-exchange-widget/static/js/2.348e2671.chunk.js"
+  },
+  {
+    "revision": "fbbf3fe2d67d775d2daa",
+    "url": "/currency-exchange-widget/static/js/main.7152be6e.chunk.js"
+  },
+  {
+    "revision": "3faaa74a960a8a677ca4",
+    "url": "/currency-exchange-widget/static/js/runtime~main.d7135418.js"
+  }
+]);
